8f420422851137779f02590cb5059c0aaa9991e7
Ivan Ustyantsev <vangold915@gmail.com>
2022-12-09 22:05:07 +0700

    Completed task 5

--- task5.py
+++ task5.py
@@ -3,3 +3,13 @@
 разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и
 выводить ее на экран.
 """
+try:
+    with open("file_for_fifth_task.txt", "w+", encoding="UTF-8") as file_object:
+        line = input("Введите цифры через пробел: ")
+        file_object.writelines(line)
+        list_of_numbers = line.split()
+        print(sum(map(int, list_of_numbers)))
+except IOError:
+    print("Ошибка ввода-вывода")
+except ValueError:
+    print("Вы ввели неправильные значения")
