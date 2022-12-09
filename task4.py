af5c5fc4bb1ac9c9355efb019f4583cebc71fe69
Ivan Ustyantsev <vangold915@gmail.com>
2022-12-09 20:33:35 +0700

    Completed task 4

--- task4.py
+++ task4.py
@@ -8,3 +8,23 @@ Four — 4
 построчно данные. При этом английские числительные должны заменяться на
 русские. Новый блок строк должен записываться в новый текстовый файл.
 """
+
+with open("file_for_fourth_task_input.txt", 'r', encoding="UTF-8") as \
+        file_object:
+    result = []
+    for line in file_object:
+        string = line.split("—")
+        if int(string[1]) == 1:
+            string[0] = "Один"
+        elif int(string[1]) == 2:
+            string[0] = "Два"
+        elif int(string[1]) == 3:
+            string[0] = "Три"
+        elif int(string[1]) == 4:
+            string[0] = "Четыре"
+        result.append(" —".join(string))
+    print(result)
+
+with open("file_for_fourth_task_output.txt", "w", encoding="UTF-8") as \
+        file_object:
+    file_object.writelines(result)
